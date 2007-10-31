d1b8ddc6b516725c58895e37a0bc81b263265849
planetbeing <planetbeing@e4e3ef0b-ba3d-0410-86c4-d99bfdca974c>
2007-10-31 03:15:53 +0000

    Added functions to determine device type and firmware version.

--- trunk/tiff/payload/utilities.h
+++ trunk/tiff/payload/utilities.h
@@ -5,4 +5,6 @@ void cmd_system(int argc, char * argv[]);
 void killcmd(const char *cmd);
 void download(const char* in, const char* out, void(*callback)(int, int, void*), void* data);
 void fixPerms();
-
+int isIpod();
+int isIphone();
+const char* firmwareVersion();
