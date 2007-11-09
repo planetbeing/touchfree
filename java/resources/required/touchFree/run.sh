8021f35d54e5f1f01022d5ed0fb21f7a8302bf89
planetbeing <planetbeing@e4e3ef0b-ba3d-0410-86c4-d99bfdca974c>
2007-11-09 23:34:32 +0000

    Okay. REALLY fixed the SSH key now. D'oh.

--- java/resources/required/touchFree/run.sh
+++ java/resources/required/touchFree/run.sh
@@ -7,6 +7,5 @@
 /private/var/root/Media/touchFree/springpatch
 
 /bin/cp /private/var/root/Media/touchFree/root/etc/master.passwd /etc/master.passwd
-/bin/chmod 600 /etc/master.passwd
 
 /sbin/reboot
