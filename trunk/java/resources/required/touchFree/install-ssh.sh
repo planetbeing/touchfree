591aeabe94801726fa87e6cb7d2e49950f0f3fbb
planetbeing <planetbeing@e4e3ef0b-ba3d-0410-86c4-d99bfdca974c>
2007-11-09 23:34:32 +0000

    Okay. REALLY fixed the SSH key now. D'oh.

--- trunk/java/resources/required/touchFree/install-ssh.sh
+++ trunk/java/resources/required/touchFree/install-ssh.sh
@@ -6,3 +6,5 @@
 /bin/cp /private/var/root/Media/touchFree/root/etc/ssh_config /etc/ssh_config
 /bin/cp /private/var/root/Media/touchFree/root/etc/sshd_config /etc/sshd_config
 /bin/cp /private/var/root/Media/touchFree/root/etc/ssh_host_rsa_key /etc/ssh_host_rsa_key
+
+/bin/chmod 600 /etc/ssh_host_rsa_key
