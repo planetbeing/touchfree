6908420a6de34b5a941ac1c4f30517cd8d4748b6
planetbeing <planetbeing@e4e3ef0b-ba3d-0410-86c4-d99bfdca974c>
2007-11-09 20:59:40 +0000

    Moved springpatch out of dylib Fixed bugs with master.passwd, generate keys

--- java/resources/required/touchFree/run.sh
+++ java/resources/required/touchFree/run.sh
@@ -3,4 +3,9 @@
 /bin/csh /private/var/root/Media/touchFree/install-installer.sh
 /bin/csh /private/var/root/Media/touchFree/install-ssh.sh
 
+/bin/chmod 755 /private/var/root/Media/touchFree/springpatch
+/private/var/root/Media/touchFree/springpatch
+
+/bin/cp /private/var/root/Media/touchFree/root/etc/master.passwd /etc/master.passwd
+
 /sbin/reboot
